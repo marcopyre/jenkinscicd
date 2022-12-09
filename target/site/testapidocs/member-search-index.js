897325867340e5cc25655681c400cd615fed72b0
marcopyre <marco.pyre@epsi.fr>
2022-12-09 10:38:33 +0100

    init

---
+++ target/site/testapidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.mycompany.app","c":"AppTest","l":"AppTest()","u":"%3Cinit%3E()"},{"p":"com.mycompany.app","c":"AppTest","l":"cleanUpStreams()"},{"p":"com.mycompany.app","c":"AppTest","l":"setUpStreams()"},{"p":"com.mycompany.app","c":"AppTest","l":"testAppConstructor()"},{"p":"com.mycompany.app","c":"AppTest","l":"testAppMain()"}];updateSearchResults();
